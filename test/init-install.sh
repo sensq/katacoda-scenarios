a60b0424886e11937f3d9722f4fa4acf2eabe591
kohari <1000.delay@gmail.com>
2021-02-08 19:17:43 +0900

    update

--- test/init-install.sh
+++ test/init-install.sh
@@ -15,7 +15,7 @@ ssh-keygen -t rsa -N "" -f ~/.ssh/test_key
 
 # Up sshd-containers(=Pseudo target server)
 cd ~/tools
-echo "PUBLIC_KEY=$(cat ~/.ssh/test_key.pub)" > .env
+echo "PUBLIC_KEY=\"$(cat ~/.ssh/test_key.pub)\"" > .env
 docker-compose up -d
 docker-compose exec target-server-01 sudo apk add python3
 docker-compose exec target-server-02 sudo apk add python3
