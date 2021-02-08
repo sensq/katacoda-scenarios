12717ba64f4304ac6a71a4b07ba5158c0183dc16
kohari <1000.delay@gmail.com>
2021-02-08 20:17:52 +0900

    update

--- test/init-install.sh
+++ test/init-install.sh
@@ -17,6 +17,3 @@ yum install docker-ce docker-ce-cli containerd.io
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-clear
-echo Done.
