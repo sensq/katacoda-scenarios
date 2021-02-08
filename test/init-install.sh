b4826f17b56d339124ecaf3e83441cf0088da27c
kohari <1000.delay@gmail.com>
2021-02-08 16:52:55 +0900

    update

--- test/init-install.sh
+++ test/init-install.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 yum install -y jq ansible ansible-lint yamllint
+
+yum install -y yum-utils
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+yum install docker-ce docker-ce-cli containerd.io
+
+curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
