90594dbcf051b5c0f670826fe6ce1c6d13e328a5
kohari <1000.delay@gmail.com>
2021-02-08 19:30:43 +0900

    update

--- test/assets/tools/setup.sh
+++ test/assets/tools/setup.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 
+# Up sshd-containers(=Pseudo target server)
+cd ~/tools
+echo "PUBLIC_KEY=\"$(cat ~/.ssh/test_key.pub)\"" > .env
+docker-compose up -d
+docker-compose exec target-server-01 sudo apk add python3
+docker-compose exec target-server-02 sudo apk add python3
--- test/init-install.sh
+++ test/init-install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Configure Prompt
+echo "export PS1=\"[\u@\h \w]\$ \"" > /etc/profile.d/init-env
+
+# Generate SSH Key
+ssh-keygen -t rsa -N "" -f ~/.ssh/test_key
+# Configure ssh-config
+cp -fp ~/configs/ssh_config ~/.ssh/config
+
+# Katacoda start directory
+cd ~/works
+
 # Install Ansible and utils
 yum install -y jq ansible ansible-lint yamllint
 # Install Docker
@@ -9,20 +20,3 @@ yum install docker-ce docker-ce-cli containerd.io
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-
-# Generate SSH Key
-ssh-keygen -t rsa -N "" -f ~/.ssh/test_key
-cp -fp ~/configs/ssh_config ~/.ssh/config
-
-# Up sshd-containers(=Pseudo target server)
-cd ~/tools
-echo "PUBLIC_KEY=\"$(cat ~/.ssh/test_key.pub)\"" > .env
-docker-compose up -d
-docker-compose exec target-server-01 sudo apk add python3
-docker-compose exec target-server-02 sudo apk add python3
-
-# Configure Prompt
-echo "export PS1=\"[\u@\h \w]\$ \"" > /etc/profile.d/init-env
-
-# Katacoda start directory
-cd ~/works
