9cd628a01e73a4ce2463e2e28741e71c8eb3475b
kohari <1000.delay@gmail.com>
2021-03-06 13:52:28 +0900

    update

--- test/init-install.sh
+++ test/init-install.sh
@@ -13,3 +13,6 @@ yum install -y jq ansible ansible-lint yamllint
 # Install docker-compose
 curl -L "https://github.com/docker/compose/releases/download/1.28.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
+
+# completed mark
+touch /completed
