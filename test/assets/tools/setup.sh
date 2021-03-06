42c28257a61b1bd2b2542ea4f8cd32e98b92e1fe
kohari <1000.delay@gmail.com>
2021-03-06 13:23:42 +0900

    add

--- test/assets/tools/setup.sh
+++ test/assets/tools/setup.sh
@@ -7,8 +7,8 @@ sleep 10
 cd /tools
 echo "PUBLIC_KEY=\"$(cat ~/.ssh/test_key.pub)\"" > .env
 docker-compose up -d
-docker-compose exec target-server-01 sudo apk add python3
-docker-compose exec target-server-02 sudo apk add python3
-docker-compose exec target-server-03 sudo apk add python3
+docker-compose exec target-server-01 sudo bash /init-container.sh &
+docker-compose exec target-server-02 sudo bash /init-container.sh &
+docker-compose exec target-server-03 sudo bash /init-container.sh
 
 clear
