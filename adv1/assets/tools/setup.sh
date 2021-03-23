4d1a26c86e68f7775c7727083f4d89c45f061071
kohari <1000.delay@gmail.com>
2021-03-24 05:14:56 +0900

    update

--- adv1/assets/tools/setup.sh
+++ adv1/assets/tools/setup.sh
@@ -12,6 +12,5 @@ echo "PUBLIC_KEY=\"$(cat ~/.ssh/test_key.pub)\"" > .env
 docker-compose up -d
 docker-compose exec target-server-01 sudo bash /container-files/init-container.sh
 docker-compose exec target-server-02 sudo bash /container-files/init-container.sh
-docker-compose exec target-server-03 sudo bash /container-files/init-container.sh
 
 clear
